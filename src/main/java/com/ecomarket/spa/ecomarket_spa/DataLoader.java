01065fa30b534730bd4b1f6e67ad507bf4dd8072
Ilvany Zambrano <il.zambranogutierrez@duocuc.cl>
2025-06-10 12:38:41 -0400

    Modificación 10/06/2025

--- src/main/java/com/ecomarket/spa/ecomarket_spa/DataLoader.java
+++ src/main/java/com/ecomarket/spa/ecomarket_spa/DataLoader.java
@@ -1,33 +1,36 @@
 package com.ecomarket.spa.ecomarket_spa;
 
-import com.ecomarket.spa.ecomarket_spa.model.*;
-import com.ecomarket.spa.ecomarket_spa.repository.*;
+import com.ecomarket.spa.ecomarket_spa.model.InventoryItem;
+import com.ecomarket.spa.ecomarket_spa.repository.InventoryRepository;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+
+import java.util.Locale;
+
 @Profile("dev")
 @Component
 public class DataLoader implements CommandLineRunner {
+
     @Autowired
     private InventoryRepository inventoryRepository;
 
+    @Override
     public void run(String... args) throws Exception {
-        Faker faker = new Faker();
-        Random random = new Random();
+        // Usar Faker con configuración regional en español
+        Faker faker = new Faker(new Locale("es"));
 
         // Generar productos
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             InventoryItem producto = new InventoryItem();
-            producto.setNombre(faker.commerce().productName());
+            producto.setNombre(faker.commerce().productName());                // Nombre de producto en español
             producto.setCantidad(faker.number().numberBetween(1, 100));
-            producto.setDescripcion(faker.lorem().sentence());
+            producto.setDescripcion(faker.commerce().material());               // Oración aleatoria en español
             producto.setPrecio(Double.parseDouble(faker.commerce().price(5.0, 200.0)));
 
+            inventoryRepository.save(producto);
         }
     }
 }
